7e490e726de069f18ec289611c12ddbb15bfcff8
Anwesh43 <anweshthecool0@gmail.com>
2019-12-01 15:49:57 +0530

    adding Canvas extension methods

--- bouncyrotlineview/src/main/java/com/anwesh/uiprojects/bouncyrotlineview/BouncyRotLineView.kt
+++ bouncyrotlineview/src/main/java/com/anwesh/uiprojects/bouncyrotlineview/BouncyRotLineView.kt
@@ -19,9 +19,34 @@ val strokeFactor : Int = 90
 val foreColor : Int = Color.parseColor("#283593")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 30
+val sizeFactor : Float = 2.9f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawRotLine(i : Int, scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.divideScale(1, 2).sinify()
+    val sc : Float = scale.divideScale(0, 2)
+    for (j in 0..1) {
+        save()
+        rotate(90f * i + 90f * sc + 45f * sf * (1f - 2 * j))
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    save()
+    translate(gap * (i + 1), h / 2)
+    val ij : Int = Math.floor((lines) * scale.toDouble()).toInt()
+    drawRotLine(ij, scale.divideScale(ij, lines), size, paint)
+    restore()
+}
 
