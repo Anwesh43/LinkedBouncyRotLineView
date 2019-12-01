f0b2ab4bda565db6ef281c4a8217517135fdd885
Anwesh43 <anweshthecool0@gmail.com>
2019-12-01 15:38:16 +0530

    adding constants and libraries

--- bouncyrotlineview/src/main/java/com/anwesh/uiprojects/bouncyrotlineview/BouncyRotLineView.kt
+++ bouncyrotlineview/src/main/java/com/anwesh/uiprojects/bouncyrotlineview/BouncyRotLineView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.bouncyrotlineview
  * Created by anweshmishra on 01/12/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
+
